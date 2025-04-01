718461084b34f37080dcf9206ce9213784d91924
sothearo-kay <sothearo.kay@techostartup.center>
2025-04-01 09:18:39 +0700

    feat(deps): add tools

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1>Hello, world</h1>
+  <div class="container">
+    <h1>Hello, world</h1>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  padding-block: 2.5rem;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
+import MotionResolver from 'motion-v/resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      dts: true,
+      resolvers: [MotionResolver()],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
