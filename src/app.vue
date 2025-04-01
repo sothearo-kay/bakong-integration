f257f9dd0a5b5fc01f0d36aa753fdffd349ca120
sothearo-kay <sothearo.kay@techostartup.center>
2025-04-01 13:47:20 +0700

    feat(ui): setup view

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="container">
-    <h1>Hello, world</h1>
+    <header></header>
+
+    <main>
+      <!-- Product List -->
+      <div></div>
+
+      <!-- Checkout Cart -->
+      <div></div>
+    </main>
+
+    <!-- KHQR Modal -->
   </div>
 </template>
 
@@ -8,5 +18,21 @@
 .container {
   min-height: 100vh;
   padding-block: 2.5rem;
+  display: grid;
+  grid-template-rows: auto 1fr;
+}
+
+main {
+  display: grid;
+  grid-template-columns: 9fr 3fr;
+  min-height: 500px;
+}
+
+main > div {
+  padding-inline: 1rem;
+}
+
+main > div:last-child {
+  border-left: 1px solid #b4c8c8;
 }
 </style>
