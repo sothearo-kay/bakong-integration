e87c84e7b37c2b8afeb51faf8024bff5bc7cc7f9
sothearo-kay <sothearo.kay@techostartup.center>
2025-04-02 10:32:21 +0700

    feat(ui): dialog example

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,5 @@
+<script setup lang="ts"></script>
+
 <template>
   <div class="container">
     <header></header>
@@ -11,6 +13,16 @@
     </main>
 
     <!-- KHQR Modal -->
+    <ui-dialog>
+      <template #trigger="{ openDialog }">
+        <button @click="openDialog">Open Dialog</button>
+      </template>
+
+      <template #content="{ closeDialog }">
+        <p>This is the dialog content.</p>
+        <button @click="closeDialog">Close</button>
+      </template>
+    </ui-dialog>
   </div>
 </template>
 
