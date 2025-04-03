30a9f22492fae6519dfedae7c9f7a027208f61b4
sothearo-kay <sothearo.kay@techostartup.center>
2025-04-03 14:48:38 +0700

    feat(dev): add prettier plugin tailwindcss

--- src/app.vue
+++ src/app.vue
@@ -1,29 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="container">
-    <header></header>
+  <main class="container">
+    <!-- Product List -->
+    <div></div>
 
-    <main>
-      <!-- Product List -->
-      <div></div>
+    <!-- Checkout Cart -->
+    <div>
+      <ui-dialog>
+        <template #trigger="{ openDialog }">
+          <button @click="openDialog">Open Dialog</button>
+        </template>
 
-      <!-- Checkout Cart -->
-      <div></div>
-    </main>
-
-    <!-- KHQR Modal -->
-    <ui-dialog>
-      <template #trigger="{ openDialog }">
-        <button @click="openDialog">Open Dialog</button>
-      </template>
-
-      <template #content="{ closeDialog }">
-        <p>This is the dialog content.</p>
-        <button @click="closeDialog">Close</button>
-      </template>
-    </ui-dialog>
-  </div>
+        <template #content="{ closeDialog }">
+          <p>This is the dialog content.</p>
+          <button @click="closeDialog">Close</button>
+        </template>
+      </ui-dialog>
+    </div>
+  </main>
 </template>
-
-<style scoped></style>
