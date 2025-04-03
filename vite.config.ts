c58ad32a561e3f569b5ab5d5de69e897c1166702
sothearo-kay <sothearo.kay@techostartup.center>
2025-04-03 14:43:30 +0700

    feat(dep): add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
 import MotionResolver from 'motion-v/resolver'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
       dts: true,
       resolvers: [MotionResolver()],
     }),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
