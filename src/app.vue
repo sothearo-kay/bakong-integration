4432c26f30d9e224027cbca1f6e2a8596c3309b7
sothearo-kay <sothearo.kay@techostartup.center>
2025-04-03 14:40:55 +0700

    chore: remove styles

--- src/app.vue
+++ src/app.vue
@@ -26,25 +26,4 @@
   </div>
 </template>
 
-<style scoped>
-.container {
-  min-height: 100vh;
-  padding-block: 2.5rem;
-  display: grid;
-  grid-template-rows: auto 1fr;
-}
-
-main {
-  display: grid;
-  grid-template-columns: 9fr 3fr;
-  min-height: 500px;
-}
-
-main > div {
-  padding-inline: 1rem;
-}
-
-main > div:last-child {
-  border-left: 1px solid #b4c8c8;
-}
-</style>
+<style scoped></style>
