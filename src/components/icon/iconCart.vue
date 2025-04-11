4d91132e7d9432a0ddb8db9851f7d4dd5db32dea
sothearo-kay <sothearo.kay@techostartup.center>
2025-04-11 09:04:32 +0700

    feat(ui): add icon

---
+++ src/components/icon/iconCart.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+export default {
+  name: "LucideShoppingCart",
+};
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+    <!-- Icon from Lucide by Lucide Contributors - https://github.com/lucide-icons/lucide/blob/main/LICENSE -->
+    <g
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <circle cx="8" cy="21" r="1"></circle>
+      <circle cx="19" cy="21" r="1"></circle>
+      <path
+        d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"
+      ></path>
+    </g>
+  </svg>
+</template>
