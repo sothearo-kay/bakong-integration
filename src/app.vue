4568ff8ef11ff992273ae783674189d7cd89faa8
sothearo-kay <sothearo.kay@techostartup.center>
2025-04-11 08:53:47 +0700

    feat: homepage layout

--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const searchTerm = ref("");
+</script>
 
 <template>
-  <main class="container">
-    <!-- Product List -->
-    <div></div>
+  <main class="container grid gap-6 py-10 md:grid-cols-3">
+    <div class="space-y-4 md:col-span-2">
+      <form @submit.prevent="">
+        <input
+          v-model="searchTerm"
+          placeholder="Search product..."
+          class="w-full rounded-lg px-4 py-2 shadow"
+        />
+      </form>
 
-    <!-- Checkout Cart -->
-    <div></div>
+      <ui-card>
+        <div>
+          <h3>Your Cart</h3>
+        </div>
+      </ui-card>
+    </div>
+
+    <ui-card>
+      <h3>Order Summary</h3>
+    </ui-card>
   </main>
 </template>
